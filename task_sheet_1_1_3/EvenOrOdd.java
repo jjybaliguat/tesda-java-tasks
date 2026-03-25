609f9830fe04f4c6034b8a693fd10460901c8314
jjybaliguat <justinejeraldbaliguat@gmail.com>
2026-03-25 17:12:02 +0800

    finished task sheet 1-1-3

---
+++ task_sheet_1_1_3/EvenOrOdd.java
@@ -0,0 +1,11 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+
+        for(int i = 1; i <= check_number; i++){
+            message =  i + ((i%2==0) ? " is even number" : " is odd number");
+            System.out.println(message);
+        }
+    }
+}
