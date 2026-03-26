68f525a1115fad6b2929853965e837211f9f91f2
jjybaliguat <justinejeraldbaliguat@gmail.com>
2026-03-26 13:39:36 +0800

    finished task2

---
+++ task2/Task2.java
@@ -0,0 +1,14 @@
+public class Task2 {
+    public static void main(String[] args) {
+        // byte, short, int, float, char, boolean
+        byte three = 3;
+        short one = 1;
+        int zero = 0;
+        float twoF = 2f;
+        char d = 'd';
+        boolean bool = true;
+        
+        System.out.print("H"+three+one+one+zero+" "+"w"+zero+"r"+one+d+" "+twoF+" "+bool);
+        // Output => H3110 w0r1d 2.0 true
+    }
+}
